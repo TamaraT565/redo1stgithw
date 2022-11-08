558b9f503366c1531c9aba2a8ce4eda0ef0a27f4
Tamara Tatarintseva <tta.nemo@gmail.com>
2022-11-08 14:33:57 +0300

    modified javafiles

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {25, 30, 12};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println(salesManager.max());
+    }
 }
